1eaf51bb0ad50aa9c3afdd8ce6a4170fe13a99cb
Vivek Manoharan <vivek.m@vuedata.in>
2022-10-12 09:51:48 +0530

    using filter and exclude methods

--- crm_project/organization/views.py
+++ crm_project/organization/views.py
@@ -1,8 +1,8 @@
-from unicodedata import name
 from django.shortcuts import render
 from .models import Hardware, Software, Employees
 from rest_framework import generics
 from .serializers import HardwareSerializer, SoftwareSerializer, EmployeesSerializer
+from django.db.models.query import Q
 
 # Create your views here.
 class CreateHardware(generics.CreateAPIView):
@@ -22,9 +22,15 @@ class ListHardware(generics.ListAPIView):
     serializer_class = HardwareSerializer
     def get_queryset(self):
         qs = Hardware.objects.all()
-        query = self.request.GET.get('q')
-        if query is not None:
-            qs = qs.filter().distinct()
+        qs = qs.filter(pk != 1)
+        qs =  qs.exclude(name = '')
+
+        #qs = [q for q in qs if q.name != '']
+        #qs = qs.filter(Q('name') != '')
+        # query = self.request.GET.get('q')
+
+        # if query is not None:
+        #     qs = qs.filter().distinct()
         return qs
 
 class DetailHardware(generics.RetrieveAPIView):
